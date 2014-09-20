7c87822ced5fcfadbce7e49f62579db8dcf908be
mathew-mcloughlin <mathew.mcloughlin@optimaenergy.net>
2014-09-20 17:11:05 +0100

    Initial commit

---
+++ CodingKata/CodingKata/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(CodingKata.Startup))]
+
+namespace CodingKata
+{
+    using System.Web.Http;
+
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
+        }
+    }
+}
